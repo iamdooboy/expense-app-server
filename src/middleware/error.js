4a758a6fdadb5c18b80f070d193a86e5d1273c2b
Duy Le <asianinvasion.le@gmail.com>
2022-08-23 12:39:15 -0400

    fixed error handler

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -1,12 +1,11 @@
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
 
     res.json({
         msg: err?.message,
         stack: process.env.NODE_ENV === 'production' ? null : err?.stack,
     });
-
-    res.status(statusCode);
 };
 
 const notFound = (req, res, next) => {
